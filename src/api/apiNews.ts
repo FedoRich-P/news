af00bb9acb4737cd7fabef003f7d06f10f9d49bf
FedoRich-P <panchev_fedor@mail.ru>
2025-03-16 09:46:20 +0300

    add pagination, change api

--- src/api/apiNews.ts
+++ src/api/apiNews.ts
@@ -3,11 +3,13 @@ import axios from "axios";
 export const BASE_URL = import.meta.env.VITE_NEWS_BASE_URL;
 export const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
-export const getNews = async () => {
+export const getNews = async ({currentPage = 1, pageSize = 10}) => {
     try {
         const response = await axios.get(`${BASE_URL}/search`, {
                 params: {
                     apiKey: API_KEY,
+                    currentPage,
+                    pageSize,
                 },
             },
         );
